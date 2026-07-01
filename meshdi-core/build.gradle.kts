48cb9ec8c810edadc4b20bd5c3a0235588d42c6f
_xBott <roma.plakhotniuk07@gmail.com>
2026-07-01 13:32:26 +0300

    added core module with project impl

---
+++ meshdi-core/build.gradle.kts
@@ -0,0 +1,20 @@
+dependencies {
+
+    implementation(project(":meshdi-api"))
+
+    implementation(libs.guava)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter)
+
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.junit.jupiter)
+
+    testImplementation(libs.assertj.core)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+
+    implementation(libs.bundles.kern)
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "meshdi"
 
 include("meshdi-api")
+include("meshdi-core")
