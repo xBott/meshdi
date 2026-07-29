e1e368cd44fec5c71631c811cf20b395e75ed52e
_xBott <roma.plakhotniuk07@gmail.com>
2026-07-29 15:01:22 +0300

    feat: added support of init strategies and scope types

--- meshdi-processor/src/main/java/me/bottdev/meshdi/processor/MeshdiMetaProcessor.java
+++ meshdi-processor/src/main/java/me/bottdev/meshdi/processor/MeshdiMetaProcessor.java
@@ -1,9 +1,14 @@
 package me.bottdev.meshdi.processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Singular;
+import me.bottdev.kern.commons.key.KeyUtils;
 import me.bottdev.kern.dependency.*;
 import me.bottdev.kern.dependency.containers.SimpleDependentContainer;
 import me.bottdev.kern.dependency.exceptions.DependencyException;
@@ -17,11 +22,16 @@ import me.bottdev.kern.meta.core.models.type.ClassModel;
 import me.bottdev.kern.struct.algorithms.cycle.SimpleCycleDetector;
 import me.bottdev.kern.struct.algorithms.sort.CircularDependencyException;
 import me.bottdev.kern.struct.algorithms.sort.KahnSorter;
+import me.bottdev.meshdi.api.ContextMesh;
+import me.bottdev.meshdi.api.InitializationStrategy;
+import me.bottdev.meshdi.api.ScopeType;
 import me.bottdev.meshdi.api.annotations.Component;
 import me.bottdev.meshdi.api.annotations.Dependency;
 import me.bottdev.meshdi.api.annotations.Inject;
+import me.bottdev.meshdi.core.builders.SimpleContextBuilder;
 
 import javax.annotation.processing.Processor;
+import javax.lang.model.element.Modifier;
 import java.util.*;
 
 import static me.bottdev.kern.meta.core.diagnostic.DiagnosticRuleResult.error;
@@ -37,16 +47,22 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
 
         private final ClassModel model;
         private final String componentKey;
+        private final InitializationStrategy initializationStrategy;
+        private final ScopeType scopeType;
         private final List<ConstructorModel> constructors;
         private final List<ConstructorModel> injectConstructors;
 
         public ComponentData(
                 ClassModel model,
                 String componentKey,
+                InitializationStrategy initializationStrategy,
+                ScopeType scopeType,
                 List<ConstructorModel> constructors
         ) {
             this.model = model;
             this.componentKey = componentKey;
+            this.initializationStrategy = initializationStrategy;
+            this.scopeType = scopeType;
             this.constructors = Collections.unmodifiableList(constructors);
             this.injectConstructors = constructors.stream()
                     .filter(constructorModel -> constructorModel.annotation(Inject.class).isPresent())
@@ -74,27 +90,31 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
 
     private static class ComponentDefinition implements DependencyAware<String> {
 
+        @Getter
+        private final ClassModel model;
         private final String dependencyKey;
-        private final List<DependencyRequest<String>> dependencies;
+        @Getter private final InitializationStrategy initializationStrategy;
+        @Getter private final ScopeType scopeType;
+        @Getter private final List<DependencyRequest<String>> dependencies;
 
         @Builder
         public ComponentDefinition(
+                ClassModel model,
                 String dependencyKey,
-                @Singular
-                List<DependencyRequest<String>> dependencies
+                InitializationStrategy initializationStrategy,
+                ScopeType scopeType,
+                @Singular List<DependencyRequest<String>> dependencies
         ) {
+            this.model = model;
             this.dependencyKey = Objects.requireNonNull(dependencyKey, "Component qualified name must be non-null");
+            this.initializationStrategy = initializationStrategy;
+            this.scopeType = scopeType;
             this.dependencies = Collections.unmodifiableList(dependencies);
         }
 
         @Override
         public String dependencyKey() {
             return dependencyKey;
-        }
-
-        @Override
-        public List<DependencyRequest<String>> getDependencies() {
-            return dependencies;
         }
 
     }
@@ -112,10 +132,18 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
 
                     String className = model.qualifiedName();
                     Component component = model.annotation(Component.class).orElseThrow();
+                    InitializationStrategy initializationStrategy = component.init();
+                    ScopeType scopeType = component.scope();
                     String qualifier = component.qualifier();
                     String componentKey = qualifier.isBlank() ? className : className + "@" + qualifier;
 
-                    return new ComponentData(model, componentKey, model.constructors());
+                    return new ComponentData(
+                            model,
+                            componentKey,
+                            initializationStrategy,
+                            scopeType,
+                            model.constructors()
+                    );
 
                 })
                 .validate(rules -> rules
@@ -130,8 +158,6 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
                         .rule(data -> discoveredComponents.add(data.getComponentKey()) ? ok() : error(data.getComponentKey() + " found duplicate component key"))
                 )
                 .map(data -> {
-
-                    String componentKey = data.getComponentKey();
 
                     ConstructorModel constructor = data.getCorrectConstructor();
                     List<DependencyRequest<String>> dependencies = constructor.parameters().stream()
@@ -163,7 +189,13 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
                             })
                             .toList();
 
-                    return new ComponentDefinition(componentKey, dependencies);
+                    return new ComponentDefinition(
+                            data.getModel(),
+                            data.getComponentKey(),
+                            data.getInitializationStrategy(),
+                            data.getScopeType(),
+                            dependencies
+                    );
 
                 })
                 .finishWith(containerBuilder::add);
@@ -183,6 +215,8 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
                                     layer.forEach(definition -> context().logger().message(MessageType.INFO, "    - " + definition.dependencyKey()));
                                 }
 
+                                createBoostrap(result);
+
                                 return ok();
 
                             } catch (DependencyException | CircularDependencyException ex) {
@@ -193,6 +227,50 @@ public class MeshdiMetaProcessor extends AbstractMetaProcessor {
                         })
                 )
                 .finish();
+
+    }
+
+    private void createBoostrap(ResolutionResult<ComponentDefinition> definitions) {
+
+        context().logger().message(MessageType.INFO, definitions.ordered().size() + " ");
+        String packageName = definitions.ordered().getFirst().getModel().packageName() + ".generated";
+
+        MethodSpec.Builder methodSpec = MethodSpec.methodBuilder("bootstrap")
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .addParameter(ContextMesh.class, "mesh")
+                .addStatement("$T builder = new $T()", SimpleContextBuilder.class, SimpleContextBuilder.class);
+
+        definitions.ordered().forEach(definition -> {
+            ClassName className = ClassName.bestGuess(definition.model.qualifiedName());
+            StringBuilder statement = new StringBuilder();
+            statement.append("builder.construct($T.key($T.class), config -> config");
+            switch (definition.getInitializationStrategy()) {
+                case LAZY -> statement.append(".lazy()");
+                case EAGER -> statement.append(".eager()");
+            }
+            switch (definition.getScopeType()) {
+                case SINGLETON -> statement.append(".singleton()");
+                case PROTOTYPE -> statement.append(".prototype()");
+            }
+            statement.append(")");
+            methodSpec.addStatement(statement.toString(), KeyUtils.class, className);
+        });
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("ContextBoostrapImpl")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(methodSpec.build())
+                .build();
+
+        JavaFile javaFile = JavaFile.builder(packageName, typeSpec)
+                .build();
+
+        try {
+            javaFile.writeTo(filer());
+
+        } catch (Exception ex) {
+            context().logger().message(MessageType.ERROR, "Failed to create ContextBootstrapImpl class.");
+
+        }
 
     }
 
