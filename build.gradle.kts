f7ae41bbee1d06824fb9f7e3eeb2d9fb3439efdc
_xBott <roma.plakhotniuk07@gmail.com>
2026-08-30 14:32:28 +0200

    configured: publishing to maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,19 @@
+import io.github.klahap.dotenv.DotEnvBuilder
+
 plugins {
     java
+    id("io.github.klahap.dotenv") version "1.1.3"
+    id("maven-publish")
+}
+
+val rootEnvVars = DotEnvBuilder.dotEnv {
+    addFile("$rootDir/.env")
 }
 
 allprojects {
 
     group = "me.bottdev.meshdi"
-    version = "0.0.1"
+    version = "0.0.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
@@ -20,9 +28,48 @@ allprojects {
 subprojects {
 
     apply(plugin = "java")
+    apply(plugin = "io.github.klahap.dotenv")
+    apply(plugin = "maven-publish")
+
+    extra["envVars"] = rootEnvVars
 
     tasks.test {
         useJUnitPlatform()
+    }
+
+    if (!project.path.startsWith(":meshdi-example")) {
+
+        publishing {
+            repositories {
+                maven {
+
+                    name = project.name
+                    url = if (version.toString().endsWith("-SNAPSHOT"))
+                        uri("https://reposlite.nimbra.net/snapshots")
+                    else
+                        uri("https://reposlite.nimbra.net/releases")
+
+                    credentials {
+                        username = rootEnvVars["REPO_USERNAME"].toString()
+                        password = rootEnvVars["REPO_PASSWORD"].toString()
+                    }
+
+                    authentication {
+                        create<BasicAuthentication>("basic")
+                    }
+                }
+            }
+
+            publications {
+                create<MavenPublication>("maven") {
+                    groupId = "me.bottdev"
+                    artifactId = project.path.substring(1).replace(":", "-")
+                    version = "${project.version}"
+                    from(components["java"])
+                }
+            }
+        }
+
     }
 
 }
