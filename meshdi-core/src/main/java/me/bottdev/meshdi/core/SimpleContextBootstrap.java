ae4801f8a0e5d3b602c71250fd3a393ee6173ea1
_xBott <roma.plakhotniuk07@gmail.com>
2026-09-01 15:25:06 +0200

    fix(core): restore TCCL during module initialization in Virtual Threads
    При попытке инициализации модуля open-telemetry (или любого другого модуля, использующего MeshDI) на сервере Paper выбрасывается необработанное исключение java.util.concurrent.CompletionException, вызванное java.util.ServiceConfigurationError.
    
    Причина:
    Внутренний механизм ServiceLoader.load, используемый в SimpleContextBootstrap.bootstrap(), падает с ClassNotFoundException (который внутри JDK оборачивается в ServiceConfigurationError). Это происходит из-за отсутствия нужного ContextClassLoader в VirtualThread. Обертка асинхронной загрузки зависимостей, которая запускает инициализацию модуля в виртуальном потоке, не имеет корректного доступа к ресурсам ModuleClassLoader, если инициализация ядра MeshDI не восстанавливает TCCL (Thread Context ClassLoader).

--- meshdi-core/src/main/java/me/bottdev/meshdi/core/SimpleContextBootstrap.java
+++ meshdi-core/src/main/java/me/bottdev/meshdi/core/SimpleContextBootstrap.java
@@ -13,8 +13,10 @@ public class SimpleContextBootstrap {
     public static SimpleContextBuilder bootstrap(ClassLoader classLoader) throws ContextBootstrapException {
 
         SimpleContextBuilder builder = new SimpleContextBuilder();
+        ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
 
         try {
+            Thread.currentThread().setContextClassLoader(classLoader);
             for (ComponentDefinition<?> definition : ServiceLoader.load(ComponentDefinition.class, classLoader)) {
                 BindingBuilder<?> bindingBuilder = definition.create();
                 builder.binding(bindingBuilder);
@@ -23,7 +25,8 @@ public class SimpleContextBootstrap {
             return builder;
         } catch (ServiceConfigurationError ex) {
             throw new ContextBootstrapException("Failed to load bean definitions.", ex);
-
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
         }
 
     }
